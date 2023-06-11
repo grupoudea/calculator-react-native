e23d72751162512675877deedc04835e2bc65450
berracode <juanguillermoalarcon@gmail.com>
2023-06-10 20:47:46 -0500

    statusbar is set in black

---
+++ src/screen/CalculatorScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { styles } from '../theme/AppTheme'
+
+const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text style={styles.texto}>calculadora</Text>
+    </View>
+  )
+}
+
+export default CalculatorScreen
